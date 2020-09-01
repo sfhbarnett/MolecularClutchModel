5ce04c2fa8cf9df0b1450e6c6ae3c541b1252209
sfhbarnett <sfhbarnett91@gmail.com>
2020-09-01 09:28:37 +0800

    Model appears to be working correctly, will run more extensive tests as next step

--- main.py
+++ main.py
@@ -2,24 +2,23 @@ import random
 import math
 import matplotlib.pyplot as plt
 
-Nmyosin = 800
+Nmyosin = 75
 Fmyosin = 2e-12
-Fs = Nmyosin*Fmyosin
-talinclutch = 1200
+Fstall = -Nmyosin * Fmyosin
+talinclutch = 75
 talin_kon = 1 # s-1
 talin_koff = 0.2
-kT = 4.11e-12
 clutch_binding_status = [0 for t in range(talinclutch)]
 force = [0 for f in range(talinclutch)]
 clutch_position = [0 for position in range(talinclutch)]
-actin_flow = -90e-9
+actin_flow = -90e-9 #nm/s
 breakingforce = 50e-12
 timepoints = 400
 Fratio = 1
 
 
 Nb = sum(clutch_binding_status)
-ksub = 2e3 #substrate stiffness, 0.01-100 pN/nm, = 0.01e-3-100e-3 N/m
+ksub = 2e-3 #substrate stiffness, 0.01-100 pN/nm, = 0.01e-3-100e-3 N/m
 xsub = 0 #attachment point of the substrate
 kclutch = 5e-3 #clutch spring constant (N/m),refered to as Kc in other models and 5 pN/nm
 ts= 5e-3 #timestep (seconds)
@@ -32,27 +31,29 @@ for i in range(timepoints):
     #calculate engagement/disengagement
     for talin in range(talinclutch):
         if clutch_binding_status[talin] == 1: #if clutch is engages
-            koff = math.exp(talin_koff*ts) * math.exp(force[talin] / Frupture)
+            koff = math.exp(-talin_koff)*ts * math.exp(force[talin] / Frupture)
             switch = random.random()
             if switch < koff: #check if clutch disengages
                 clutch_binding_status[talin] = 0
         else: #check if clutch becomes engaged
             switch = random.random()
-            if switch > math.exp(-talin_kon*ts):
+            if switch > math.exp(-talin_kon)*ts:
                 clutch_binding_status[talin] = 1
 
     # balance forces on the clutches by stretching the substrate
     xsub = (kclutch * sum([pos * bound for pos, bound in zip(clutch_position, clutch_binding_status)])) / (ksub + sum(clutch_binding_status) * kclutch)
     #update actin flow rate
-    truevelocity = actin_flow * (1 - (ksub * xsub) / Fs)
-    #Calculate new clutch positions for engaged clutches
-    clutch_position = [(cp+truevelocity)*bs*ts if bs == 1 else xsub for cp,bs in zip(clutch_position,clutch_binding_status)]
+    truevelocity = actin_flow * (1 - (ksub * xsub) / Fstall)
 
-    print(clutch_position)
+    #Calculate new clutch positions for engaged clutches
+    clutch_position = [(cp+truevelocity*bs*ts) if bs == 1 else xsub for cp,bs in zip(clutch_position,clutch_binding_status)]
+
+    #print(clutch_position)
     filament.append(truevelocity*1e9)
-    SubstrateForce.append(sum(force))
+    SubstrateForce.append(xsub*ksub)
     #Update the force on each clutch for the new substrate position
-    force = [kclutch*(cp-xsub) for cp in clutch_position]
+    force = [kclutch*(abs(cp-xsub)) for cp in clutch_position]
+    #print(SubstrateForce)
 
 print(SubstrateForce)
 plt.plot(filament)
